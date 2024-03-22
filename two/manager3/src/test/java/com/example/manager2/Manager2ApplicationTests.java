3a786c2377a22cc3f4df810a43db9f906b48e9d6
praanta <prashant.antagonist@gmail.com>
2024-03-22 19:45:22 +0530

    second commit

---
+++ two/manager3/src/test/java/com/example/manager2/Manager2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manager2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Manager2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ two/manager4/src/main/java/com/example/manager2/Manager2Application.java
@@ -0,0 +1,13 @@
+package com.example.manager2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Manager2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Manager2Application.class, args);
+	}
+
+}
